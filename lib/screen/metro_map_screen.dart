a89a7f8c2413a1ef72051e0b9f75d317c13dafc6
rahul <rkrahulroy151617@gmail.com>
2025-10-27 21:30:17 +0530

    bug fix

--- lib/screen/metro_map_screen.dart
+++ lib/screen/metro_map_screen.dart
@@ -1,5 +1,5 @@
-// lib/screens/metro_map_screen.dart
 import 'package:flutter/material.dart';
+import 'package:patna_metro/utils/app_color.dart';
 import 'package:photo_view/photo_view.dart';
 
 class MetroMapScreen extends StatelessWidget {
@@ -7,6 +7,7 @@ class MetroMapScreen extends StatelessWidget {
   final String imagePath;
 
   const MetroMapScreen({
+    super.key,
     required this.title,
     required this.imagePath,
   });
@@ -15,62 +16,50 @@ class MetroMapScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
-        backgroundColor: Colors.blue[700],
-        actions: [
-          IconButton(
-            icon: Icon(Icons.zoom_in),
-            onPressed: () {
-              // Zoom functionality handled by PhotoView
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.zoom_out),
-            onPressed: () {
-              // Zoom functionality handled by PhotoView
-            },
-          ),
-        ],
+        title: Text(title, style: TextStyle(color: Colors.white)),
+        backgroundColor: AppColor.primaryColor,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white70),
+        ),
       ),
-      body: Container(
-        color: Colors.grey[100],
-        child: PhotoView(
-          imageProvider: AssetImage(imagePath),
-          minScale: PhotoViewComputedScale.contained * 0.8,
-          maxScale: PhotoViewComputedScale.covered * 3.0,
-          initialScale: PhotoViewComputedScale.contained,
-          backgroundDecoration: BoxDecoration(
-            color: Colors.grey[100],
+      backgroundColor: Colors.white,
+      body: PhotoView(
+        imageProvider: AssetImage(imagePath),
+        minScale: PhotoViewComputedScale.contained * 0.8,
+        maxScale: PhotoViewComputedScale.covered * 3.0,
+        initialScale: PhotoViewComputedScale.contained,
+        backgroundDecoration: BoxDecoration(color: Colors.grey[100]),
+        loadingBuilder: (context, event) => Center(
+          child: SizedBox(
+            width: 50.0,
+            height: 50.0,
+            child: CircularProgressIndicator(
+              value: event == null
+                  ? 0
+                  : event.cumulativeBytesLoaded / event.expectedTotalBytes!,
+            ),
           ),
-          loadingBuilder: (context, event) => Center(
-            child: Container(
-              width: 50.0,
-              height: 50.0,
-              child: CircularProgressIndicator(
-                value: event == null
-                    ? 0
-                    : event.cumulativeBytesLoaded / event.expectedTotalBytes!,
+        ),
+        errorBuilder: (context, error, stackTrace) => Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.error_outline, color: Colors.red, size: 50),
+              SizedBox(height: 16),
+              Text(
+                'Map image not found',
+                style: TextStyle(fontSize: 16, color: Colors.grey[600]),
               ),
-            ),
-          ),
-          errorBuilder: (context, error, stackTrace) => Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.error_outline, color: Colors.red, size: 50),
-                SizedBox(height: 16),
-                Text(
-                  'Map image not found',
-                  style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Please check if the image exists at: $imagePath',
-                  style: TextStyle(fontSize: 12, color: Colors.grey[500]),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
+              SizedBox(height: 8),
+              Text(
+                'Please check if the image exists at: $imagePath',
+                style: TextStyle(fontSize: 12, color: Colors.grey[500]),
+                textAlign: TextAlign.center,
+              ),
+            ],
           ),
         ),
       ),
